fa50ef0c8521969d516ad32960b0be3ed9ec8002
Guz117 <cristian.guzman117@gmail.com>
2021-11-19 16:20:17 +0100

    stampa in html

--- js/script.js
+++ js/script.js
@@ -12,18 +12,26 @@ let etaPasseggero = parseInt(prompt('Inserisci inserire età'));
 console.log('Età:', etaPasseggero);
 
 let prezzoBiglietto = (0.21 * kmPercorrere).toFixed(2);
-// console.log('Prezzo del biglietto', prezzoBiglietto);
+// console.log('Prezzo del biglietto:', prezzoBiglietto);
 
 let prezzoBigliettoMinorenne = ((0.21 - (0.21 * 20 / 100)) * kmPercorrere).toFixed(2);
-// console.log('Prezzo del biglietto per minorenne', prezzoBigliettoMinorenne);
+// console.log('Prezzo del biglietto per minorenne:', prezzoBigliettoMinorenne);
 
 let prezzoBigliettoOver = ((0.21 - (0.21 * 40 / 100)) * kmPercorrere).toFixed(2);
-// console.log('Prezzo del biglietto per over 65', prezzoBigliettoOver);
+// console.log('Prezzo del biglietto per over 65:', prezzoBigliettoOver);
 
 if (etaPasseggero < 18) {
-    console.log('Prezzo del biglietto per minorenne', prezzoBigliettoMinorenne);
+    console.log('Prezzo del biglietto per minorenne:', prezzoBigliettoMinorenne);
 } else if (etaPasseggero > 65) {
-    console.log('Prezzo del biglietto per over 65', prezzoBigliettoOver);
+    console.log('Prezzo del biglietto per over 65:', prezzoBigliettoOver);
 } else {
-    console.log('Prezzo del biglietto', prezzoBiglietto);
+    console.log('Prezzo del biglietto:', prezzoBiglietto);
 }
+
+if (etaPasseggero < 18) {
+    document.writeln("Il prezzo del biglietto è: ", prezzoBigliettoMinorenne + " €");
+} else if (etaPasseggero > 65) {
+    document.writeln("Il prezzo del biglietto è: ", prezzoBigliettoOver + " €");
+} else {
+    document.writeln("Il prezzo del biglietto è: ", prezzoBiglietto + " €");
+}   
