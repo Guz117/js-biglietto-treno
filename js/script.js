ccb3172e4d9f5a3d065a48bab9481094c82f1f0b
Guz117 <cristian.guzman117@gmail.com>
2021-11-22 09:58:40 +0100

    parseFloat

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+/* 
+    Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+    Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+    il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    va applicato uno sconto del 20% per i minorenni
+    va applicato uno sconto del 40% per gli over 65.
+    l’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+*/
 
-let kmPercorrere = parseInt(prompt('Inserire il numero di km che si vogliono percorrere'));
+let kmPercorrere = parseFloat(prompt('Inserire il numero di km che si vogliono percorrere'));
 console.log('Numero di km che si vogliono percorrere:', kmPercorrere);
 
 let etaPasseggero = parseInt(prompt('Inserisci inserire età'));
@@ -15,6 +17,7 @@ let prezzoBiglietto = (0.21 * kmPercorrere).toFixed(2);
 // console.log('Prezzo del biglietto:', prezzoBiglietto);
 
 let prezzoBigliettoMinorenne = ((0.21 - (0.21 * 20 / 100)) * kmPercorrere).toFixed(2);
+// let prezzoBigliettoMinorenne = (prezzoBiglietto * (1 - (scontoMinorenne / 100)).toFixed(2);
 // console.log('Prezzo del biglietto per minorenne:', prezzoBigliettoMinorenne);
 
 let prezzoBigliettoOver = ((0.21 - (0.21 * 40 / 100)) * kmPercorrere).toFixed(2);
@@ -35,3 +38,5 @@ if (etaPasseggero < 18) {
 } else {
     document.writeln("Il prezzo del biglietto è: ", prezzoBiglietto + " €");
 }   
+
+
